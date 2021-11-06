98bb516ee526470a617ede5893395218e53a2f1f
midbel <brun.nicolas21@gmail.com>
2021-11-06 16:02:36 +0100

    State interface + two implementations

--- internal/state/state.go
+++ internal/state/state.go
@@ -10,27 +10,76 @@ var (
 	ErrFound = errors.New("not found")
 )
 
+const (
+	keyLoop     = "loop"
+	keyLoop0    = "loop0"
+	KeyRevLoop  = "revloop"
+	KeyRevLoop0 = "revloop0"
+	keyLength   = "length"
+)
+
 type FuncMap map[string]interface{}
 
-type State struct {
-	parent  *State
-	current reflect.Value
-	filters map[string]interface{}
+type State interface {
+	Lookup(name string) (reflect.Value, error)
+	Define(name string, value reflect.Value) error
+	Resolve(name string) (reflect.Value, error)
 }
 
-func EmptyState(data interface{}, filters FuncMap) *State {
-	return EnclosedState(data, nil, filters)
+type loopState struct {
+	State
+	loop   int
+	length int
 }
 
-func EnclosedState(data interface{}, parent *State, filters FuncMap) *State {
-	return &State{
-		current: valueOf(data),
-		parent:  parent,
-		filters: filters,
+func (s *loopState) Resolve(name string) (reflect.Value, error) {
+	switch name {
+	case keyLength:
+		return reflect.ValueOf(s.length)
+	case keyLoop:
+		return reflect.ValueOf(s.loop + 1)
+	case keyLoop0:
+		return reflect.ValueOf(s.loop)
+	case keyRevLoop:
+	case keyRevLoop0:
+	default:
+		return s.State.Resolve(name)
 	}
 }
 
-func (s *State) Lookup(name string) (reflect.Value, error) {
+func (s *loopState) Define(name string, value reflect.Value) error {
+	switch name {
+	case keyLength:
+	case keyLoop:
+	case keyLoop0:
+	case keyRevLoop:
+	case keyRevLoop0:
+		return s.State.Define(name, value)
+	}
+	return fmt.Errorf("%s can not be defined", name)
+}
+
+type stdState struct {
+	parent  State
+	current reflect.Value
+	filters map[string]interface{}
+	locals  map[string]reflect.Value
+}
+
+func EmptyState(data interface{}, filters FuncMap) State {
+	return EnclosedState(data, nil, filters)
+}
+
+func EnclosedState(data interface{}, parent State, filters FuncMap) State {
+	return &stdState{
+		current: valueOf(data),
+		parent:  parent,
+		filters: filters,
+		locals:  make(map[string]reflect.Value),
+	}
+}
+
+func (s *stdState) Lookup(name string) (reflect.Value, error) {
 	if s.filters == nil && s.parent != nil {
 		return s.parent.Lookup(name)
 	}
@@ -44,19 +93,29 @@ func (s *State) Lookup(name string) (reflect.Value, error) {
 	return fn, nil
 }
 
-func (s *State) Resolve(key string) (reflect.Value, error) {
+func (s *stdState) Define(key string, value reflect.Value) error {
+	s.locals[key] = value
+	return nil
+}
+
+func (s *stdState) Resolve(key string) (reflect.Value, error) {
 	v, err := s.find(key)
+	if err != nil {
+		if r, ok := s.locals[key]; ok {
+			return r, nil
+		}
+	}
 	if err != nil && s.parent != nil {
-		v, err = s.parent.find(key)
+		v, err = s.parent.Resolve(key)
 	}
 	return v, err
 }
 
-func (s *State) find(key string) (reflect.Value, error) {
+func (s *stdState) find(key string) (reflect.Value, error) {
 	return s.findWith(key, s.current)
 }
 
-func (s *State) findWith(key string, value reflect.Value) (reflect.Value, error) {
+func (s *stdState) findWith(key string, value reflect.Value) (reflect.Value, error) {
 	switch value.Kind() {
 	case reflect.Struct:
 		return s.lookupStruct(key, value)
@@ -70,7 +129,7 @@ func (s *State) findWith(key string, value reflect.Value) (reflect.Value, error)
 	return Invalid, ErrFound
 }
 
-func (s *State) lookupStruct(key string, value reflect.Value) (reflect.Value, error) {
+func (s *stdState) lookupStruct(key string, value reflect.Value) (reflect.Value, error) {
 	t := value.Type()
 	for i := 0; i < value.NumField(); i++ {
 		sf := t.Field(i)
@@ -81,7 +140,7 @@ func (s *State) lookupStruct(key string, value reflect.Value) (reflect.Value, er
 	return Invalid, ErrFound
 }
 
-func (s *State) lookupMap(key string, value reflect.Value) (reflect.Value, error) {
+func (s *stdState) lookupMap(key string, value reflect.Value) (reflect.Value, error) {
 	t := value.Type().Key()
 	if !t.AssignableTo(reflect.TypeOf(key)) {
 		return Invalid, ErrFound
